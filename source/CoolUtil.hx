5586ca814116d5b257581ab8bd00ee72aaf56e10
Elfechijuega <96920703+Elfechijuega@users.noreply.github.com>
2022-01-18 14:35:40 -0600

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -20,6 +20,8 @@ class CoolUtil
 		'Easy',
 		'Normal',
 		'Hard'
+		'erect'
+		'd-sides'
 	];
 	public static var defaultDifficulty:String = 'Normal'; //The chart that has no suffix and starting difficulty on Freeplay/Story Mode
 
